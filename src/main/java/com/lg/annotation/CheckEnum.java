64edbab08ac3fc3f2fcc279f3529c3edeb7ab5b5
xuligang <429795986@qq.com>
2019-09-19 16:05:23 +0800

    study

--- src/main/java/com/lg/annotation/CheckEnum.java
+++ src/main/java/com/lg/annotation/CheckEnum.java
@@ -27,6 +27,12 @@ import static java.lang.annotation.ElementType.*;
 @Constraint(validatedBy = {CheckEnumValidator.class})
 public @interface CheckEnum {
 
+    String GET_BY_CODE = "getCode";
+
+    String GET_BY_NAME = "name";
+
+    String GET_BY_VALUE_OF = "valueOf";
+
     String message() default "invalid enum value.";
 
     Class<?>[] groups() default {};
@@ -55,7 +61,7 @@ public @interface CheckEnum {
      *
      * @return the method name of get the enum value
      */
-    String getter() default "getCode";
+    String getter() default GET_BY_CODE;
 
     @Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER})
     @Retention(RetentionPolicy.RUNTIME)
@@ -63,48 +69,5 @@ public @interface CheckEnum {
     @interface List {
         CheckEnum[] value();
     }
-
-    /*
-    @Documented
-    @Constraint(validatedBy = { CheckEnumValidator.class })
-    @Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER })
-    @Retention(RUNTIME)
-    @ReportAsSingleViolation
-    public @interface CheckEnum {
-
-        @Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER })
-        @Retention(RUNTIME)
-        @Documented
-        public @interface List {
-            CheckEnum[] value();
-        }
-
-        // ~~ 获取枚举值的方法
-        public static final String GET_BY_CODE = "getCode";
-
-        public static final String GET_BY_NAME = "name";
-
-        // 如果检查的枚举为一个String类型, 则根据该属性值获取String映射的枚举值
-        // 金融云2.0, 要求枚举全部含有code属性, 所以默认使用`getCode`比较.
-        // 如果没有code属性, 可以填写`valueOf`, 表示只根据`valueOf`属性比较.
-        String getter() default GET_BY_CODE;
-
-        // 检查分组
-        Class<?>[] groups() default {};
-
-        // 错误文案
-        String message() default "invalid enum value.";
-
-        Class<? extends Payload>[] payload() default {};
-
-        // ~~ 枚举值定义
-
-        // 检查的枚举的Class类型, 如果只允许其中的某几种类型, 则可以通过 whitelist() 设置
-        Class<? extends Enum> value();
-
-        // 白名单列表
-        String[] whitelist() default {};
-    }
-    */
 
 }
